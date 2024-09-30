962d8748b57d9b6c3cf0fb325491cbf19f3f08dc
jesse-barbosa <barbosajesse419@gmail.com>
2024-09-30 20:44:39 -0300

    Organizando estrutura das pastas

---
+++ include/footer.php
@@ -0,0 +1,5 @@
+<div class="py-2">
+    <div class="container">
+        Todos os Direitos Reservados &copy; 2024
+    </div>
+</div>
---
+++ telas/index.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+if (!isset($_SESSION['nome'])) {
+    header("Location: ../index.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SenacTec</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+    <?php
+    include_once("../include/header.php");
+    ?>
+    <?php
+    include_once("../classe/TrocarUrl.php");
+        $url = new TrocarUrl();
+        $url->trocarUrl(@$_GET["tela"]);
+    ?>
+    <?php
+        include_once("../include/footer.php");
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
