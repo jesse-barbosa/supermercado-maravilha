ebbb2daafc5b1e26f6e3f8ecb12e891898fba121
jesse-barbosa <barbosajesse419@gmail.com>
2024-10-02 17:30:16 -0300

    Novos dados para teste

--- banco/dbsupermarket.sql
+++ banco/dbsupermarket.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 02/10/2024 às 02:00
+-- Tempo de geração: 02-Out-2024 às 22:29
 -- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Banco de dados: `db_senactec`
+-- Banco de dados: `dbsupermarket`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `carts`
+-- Estrutura da tabela `carts`
 --
 
 CREATE TABLE `carts` (
@@ -37,7 +37,7 @@ CREATE TABLE `carts` (
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `categories`
+-- Estrutura da tabela `categories`
 --
 
 CREATE TABLE `categories` (
@@ -47,7 +47,7 @@ CREATE TABLE `categories` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Despejando dados para a tabela `categories`
+-- Extraindo dados da tabela `categories`
 --
 
 INSERT INTO `categories` (`id`, `name`, `description`) VALUES
@@ -65,7 +65,7 @@ INSERT INTO `categories` (`id`, `name`, `description`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `orders`
+-- Estrutura da tabela `orders`
 --
 
 CREATE TABLE `orders` (
@@ -76,10 +76,17 @@ CREATE TABLE `orders` (
   `status` enum('0','1','2') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
+--
+-- Extraindo dados da tabela `orders`
+--
+
+INSERT INTO `orders` (`id`, `user_id`, `product_id`, `quantity`, `status`) VALUES
+(1, 3, 10, 4, '0');
+
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `products`
+-- Estrutura da tabela `products`
 --
 
 CREATE TABLE `products` (
@@ -94,44 +101,52 @@ CREATE TABLE `products` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
--- Despejando dados para a tabela `products`
+-- Extraindo dados da tabela `products`
 --
 
 INSERT INTO `products` (`id`, `name`, `description`, `price`, `in_stock`, `image`, `category_id`, `status`) VALUES
-(1, 'Macarrão Espaguete', 'Macarrão espaguete fresco, 500g', 5.99, 100, 'macarrao_espaguete.jpg', 1, 'ATIVO'),
-(2, 'Leite Integral', 'Leite integral fresco, 1L', 3.99, 50, 'leite_integral.jpg', 2, 'ATIVO'),
-(3, 'Frango Grelhado', 'Frango grelhado fresco, 1kg', 12.99, 20, 'frango_grelhado.jpg', 3, 'ATIVO'),
-(4, 'Maçã Gala', 'Maçã gala fresca, 1kg', 6.99, 30, 'maca_gala.jpg', 4, 'ATIVO'),
-(5, 'Coca-Cola', 'Bebida refrigerante, 2L', 4.99, 50, 'coca_cola.jpg', 5, 'ATIVO'),
-(6, 'Pão Francês', 'Pão francês fresco, 500g', 2.99, 20, 'pao_frances.jpg', 6, 'ATIVO'),
-(7, 'Detergente Líquido', 'Detergente líquido para roupa, 1L', 8.99, 10, 'detergente_liquido.jpg', 7, 'ATIVO'),
-(8, 'Shampoo', 'Shampoo para cabelo, 500ml', 9.99, 15, 'shampoo.jpg', 8, 'ATIVO'),
-(9, 'Ração para Cachorro', 'Ração para cachorro, 5kg', 29.99, 5, 'racao_cachorro.jpg', 9, 'ATIVO'),
-(10, 'Bolo de Chocolate', 'Bolo de chocolate fresco, 1kg', 19.99, 10, 'bolo_chocolate.jpg', 6, 'ATIVO');
+(1, 'Macarrão Espaguete', 'Macarrão espaguete fresco, 500g', 5.00, 100, '/supermarket/img/products/macarrao_espaguete.jpg', 1, 'ATIVO'),
+(2, 'Leite Integral', 'Leite integral fresco, 1L', 3.00, 50, '/supermarket/img/products/leite_integral.jpeg', 2, 'ATIVO'),
+(3, 'Frango', 'Frango grelhado fresco, 1kg', 12.00, 20, '/supermarket/img/products/frango.jpg', 3, 'ATIVO'),
+(4, 'Maçã Gala', 'Maçã gala fresca, 1kg', 6.00, 30, '/supermarket/img/products/maca.jpg', 4, 'ATIVO'),
+(5, 'Coca-Cola', 'Bebida refrigerante, 2L', 4.00, 50, '/supermarket/img/products/coca_cola.png', 5, 'ATIVO'),
+(6, 'Pão Francês', 'Pão francês fresco, 500g', 2.00, 20, '/supermarket/img/products/pao_frances.jpg', 6, 'ATIVO'),
+(7, 'Detergente Líquido', 'Detergente líquido para roupa, 1L', 8.00, 10, '/supermarket/img/products/detergente.jpg', 7, 'ATIVO'),
+(8, 'Shampoo', 'Shampoo para cabelo, 500ml', 9.00, 15, '/supermarket/img/products/shampoo.jpg', 8, 'ATIVO'),
+(9, 'Ração para Cachorro', 'Ração para cachorro, 5kg', 29.00, 5, '/supermarket/img/products/racao_cachorro.jpg', 9, 'ATIVO'),
+(10, 'Bolo de Chocolate', 'Bolo de chocolate fresco, 1kg', 19.00, 10, '/supermarket/img/products/bolo_chocolate.jpg', 6, 'ATIVO');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `users`
+-- Estrutura da tabela `users`
 --
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(200) NOT NULL,
   `password` char(30) NOT NULL,
-  `access_level` enum('0','1','2','3') NOT NULL,
   `email` varchar(255) NOT NULL,
+  `access_level` int(1) NOT NULL,
   `cpf` varchar(11) NOT NULL,
   `phone` varchar(11) DEFAULT NULL,
   `status` char(10) NOT NULL DEFAULT 'ATIVO'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
+-- Extraindo dados da tabela `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `password`, `email`, `access_level`, `cpf`, `phone`, `status`) VALUES
+(3, 'admin-master', '1234', 'barbosajesse419@gmail.com', 2, '11111111111', '11111111111', 'ATIVO'),
+(7, 'José', '1234', 'jose@gmail.com', 1, '11111111111', '11111111111', 'ATIVO');
+
+--
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `carts`
+-- Índices para tabela `carts`
 --
 ALTER TABLE `carts`
   ADD PRIMARY KEY (`id`),
@@ -139,13 +154,13 @@ ALTER TABLE `carts`
   ADD KEY `product_id` (`product_id`);
 
 --
--- Índices de tabela `categories`
+-- Índices para tabela `categories`
 --
 ALTER TABLE `categories`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `orders`
+-- Índices para tabela `orders`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
@@ -153,20 +168,20 @@ ALTER TABLE `orders`
   ADD KEY `product_id` (`product_id`);
 
 --
--- Índices de tabela `products`
+-- Índices para tabela `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_products_categories` (`category_id`);
 
 --
--- Índices de tabela `users`
+-- Índices para tabela `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT para tabelas despejadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
@@ -179,46 +194,46 @@ ALTER TABLE `carts`
 -- AUTO_INCREMENT de tabela `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de tabela `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de tabela `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- Restrições para tabelas despejadas
+-- Restrições para despejos de tabelas
 --
 
 --
--- Restrições para tabelas `carts`
+-- Limitadores para a tabela `carts`
 --
 ALTER TABLE `carts`
   ADD CONSTRAINT `carts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `carts_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 
 --
--- Restrições para tabelas `orders`
+-- Limitadores para a tabela `orders`
 --
 ALTER TABLE `orders`
   ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   ADD CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
 
 --
--- Restrições para tabelas `products`
+-- Limitadores para a tabela `products`
 --
 ALTER TABLE `products`
   ADD CONSTRAINT `fk_products_categories` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`);
