52f2deecb12eee0e48bf5ace6880e47e1f57e919
jesse-barbosa <barbosajesse419@gmail.com>
2024-10-03 09:04:46 -0300

    Ajustes no Front-end

--- include/header.php
+++ include/header.php
@@ -1,7 +1,7 @@
 <header class="header">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container">
-    <a class="navbar-brand me-5 fw-bold fs-4" href="#"><img src="../img/logo.svg" height="30px" alt=""></a>
+    <a class="navbar-brand me-5 fw-bold fs-4" href="index.php"><img src="../img/logo.svg" height="30px" alt=""></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
